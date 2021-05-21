48ed6ea9176f9e24fd7af92325350a4f9879f613
Icaka <ico.ezekiev@gmail.com>
2021-05-21 13:42:55 +0300

    Adding the Game class

---
+++ src/Game.hx
@@ -0,0 +1,21 @@
+class Game
+{
+    private var field = [for (i in 0...40) 0];
+    private var pawnsInHouse = {red: 4, yellow: 4, blue: 4, green: 4};
+    
+    public function new() {
+        
+    }
+
+    public function printField() {
+        for(i in field)
+            Sys.println(i);
+    }
+
+    public function printPawnsInHouse() {
+        Sys.println('Red: ${pawnsInHouse.red}');
+        Sys.println('Yellow: ${pawnsInHouse.yellow}');
+        Sys.println('Blue: ${pawnsInHouse.blue}');
+        Sys.println('Green: ${pawnsInHouse.green}');
+    }
+}
---
+++ src/Main.hx
@@ -0,0 +1,9 @@
+import Game;
+
+class Main {
+	static function main() {
+		//trace("Hello, world!");
+		var game = new Game();
+		game.printPawnsInHouse();
+	}
+}
